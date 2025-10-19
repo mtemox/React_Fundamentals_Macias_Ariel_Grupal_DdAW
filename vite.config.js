eac800201ea33624047cba9b354f00b667b8ddbf
Ariel Macias <materchico14@gmail.com>
2025-10-19 16:02:12 -0500

    deploy

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/React_Fundamentals_Macias_Ariel_Grupal_DdAW/',
 })
